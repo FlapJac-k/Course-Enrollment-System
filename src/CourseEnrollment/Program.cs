9183138a11eb3434e9fc6a34d8fee251a22d8998
FlapJac-k <eslamce122@gmail.com>
2025-12-08 23:12:33 +0200

    Installed EntityFrameworkCore packages, added ApplicationDbContext and configured in-memorydb

--- src/CourseEnrollment/Program.cs
+++ src/CourseEnrollment/Program.cs
@@ -1,7 +1,14 @@
+using CourseEnrollment.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("CourseEnrollmentDb"));
+
 
 var app = builder.Build();
 
