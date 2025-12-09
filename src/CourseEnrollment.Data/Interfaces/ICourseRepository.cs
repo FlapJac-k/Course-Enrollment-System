78931e7aa4e161ce4fef8241c6f9bfa45e952773
FlapJac-k <eslamce122@gmail.com>
2025-12-10 01:21:04 +0200

    Added CourseRepository and ICourseRepository interface

---
+++ src/CourseEnrollment.Data/Interfaces/ICourseRepository.cs
@@ -0,0 +1,8 @@
+﻿using CourseEnrollment.Data.Entities;
+
+namespace CourseEnrollment.Data.Interfaces
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+    }
+}
---
+++ src/CourseEnrollment.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,31 @@
+﻿using CourseEnrollment.Data.Entities;
+using CourseEnrollment.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseEnrollment.Data.Repositories
+{
+    public class CourseRepository : Repository<Course>, ICourseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CourseRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+
+        }
+
+        public override async Task<Course?> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<Course>()
+                .Include(c => c.Enrollments)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public override async Task<IEnumerable<Course>> GetAllAsync()
+        {
+            return await _context.Set<Course>()
+                .Include(c => c.Enrollments)
+                .ToListAsync();
+        }
+    }
+}
