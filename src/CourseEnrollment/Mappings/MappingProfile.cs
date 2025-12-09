a66d36ac969efdfdc5482cba58a0ec4e3fc447a7
FlapJac-k <eslamce122@gmail.com>
2025-12-09 22:56:04 +0200

    Add mappings for Student and Course to SelectListItem

--- src/CourseEnrollment/Mappings/MappingProfile.cs
+++ src/CourseEnrollment/Mappings/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CourseEnrollment.Data.Entities;
 using CourseEnrollment.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CourseEnrollment.Mappings
 {
@@ -24,6 +25,15 @@ namespace CourseEnrollment.Mappings
                 .ForMember(dest => dest.StudentName, opt => opt.MapFrom(src => src.Student.FullName))
                 .ForMember(dest => dest.CourseTitle, opt => opt.MapFrom(src => src.Course.Title))
                 .ForMember(dest => dest.AvailableSlots, opt => opt.Ignore());
+
+            CreateMap<Student, SelectListItem>()
+            .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Id.ToString()))
+            .ForMember(dest => dest.Text, opt => opt.MapFrom(src => $"{src.FullName} ({src.Email})"));
+
+            // Course → SelectListItem
+            CreateMap<Course, SelectListItem>()
+                .ForMember(dest => dest.Value, opt => opt.MapFrom(src => src.Id.ToString()))
+                .ForMember(dest => dest.Text, opt => opt.MapFrom(src => $"{src.Title} (Available: {src.MaximumCapacity - src.Enrollments.Count})"));
         }
     }
 }
