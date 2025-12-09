5bd2e0bb3646bd56165769936c750c00af0f1c97
FlapJac-k <eslamce122@gmail.com>
2025-12-09 22:25:48 +0200

    Added view models for course enrollment management

---
+++ src/CourseEnrollment/Models/CreateEnrollmentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseEnrollment.Models
+{
+    public class CreateEnrollmentViewModel
+    {
+        [Required(ErrorMessage = "Student is required")]
+        [Display(Name = "Student")]
+        public Guid StudentId { get; set; }
+
+        [Required(ErrorMessage = "Course is required")]
+        [Display(Name = "Course")]
+        public Guid CourseId { get; set; }
+
+        public List<SelectListItem> Students { get; set; } = new();
+        public List<SelectListItem> Courses { get; set; } = new();
+    }
+}
---
+++ src/CourseEnrollment/Models/EnrollmentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseEnrollment.Models
+{
+    public class EnrollmentViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Student is required")]
+        [Display(Name = "Student")]
+        public Guid StudentId { get; set; }
+
+        [Required(ErrorMessage = "Course is required")]
+        [Display(Name = "Course")]
+        public Guid CourseId { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public required string StudentName { get; set; }
+        public required string CourseTitle { get; set; }
+        public required int AvailableSlots { get; set; }
+    }
+}
