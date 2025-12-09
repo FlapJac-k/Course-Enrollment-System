0a4cd0cfee6f9f339c40846dc03bbe4c2afad41a
FlapJac-k <eslamce122@gmail.com>
2025-12-10 00:55:04 +0200

    Added IStudentRepository and EmailExistsAsync method

---
+++ src/CourseEnrollment.Data/Interfaces/IStudentRepository.cs
@@ -0,0 +1,9 @@
+﻿using CourseEnrollment.Data.Entities;
+
+namespace CourseEnrollment.Data.Interfaces
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+        Task<bool> EmailExistsAsync(string email, Guid? excludeId = null);
+    }
+}
---
+++ src/CourseEnrollment.Data/Repositories/StudentRepository.cs
@@ -0,0 +1,25 @@
+﻿using CourseEnrollment.Data.Entities;
+using CourseEnrollment.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseEnrollment.Data.Repositories
+{
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public StudentRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> EmailExistsAsync(string email, Guid? excludeId = null)
+        {
+            var query = _context.Set<Student>().Where(s => s.Email == email);
+            if (excludeId.HasValue)
+            {
+                query = query.Where(s => s.Id != excludeId.Value);
+            }
+            return await query.AnyAsync();
+        }
+    }
+}
