5f15301c0ebe1ae9fd8d9b8e527dc52372799b1e
FlapJac-k <eslamce122@gmail.com>
2025-12-09 21:00:18 +0200

    Added AutoMapper and MappingProfile; configured mapping between Student and StudentViewModel

---
+++ src/CourseEnrollment/Mappings/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CourseEnrollment.Data.Entities;
+using CourseEnrollment.Models;
+
+namespace CourseEnrollment.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Student, StudentViewModel>();
+            CreateMap<StudentViewModel, Student>();
+        }
+    }
+}
