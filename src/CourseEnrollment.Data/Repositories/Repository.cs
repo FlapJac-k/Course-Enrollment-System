967eb62125de3938ca504443734685655a7fa3b6
FlapJac-k <eslamce122@gmail.com>
2025-12-09 21:30:39 +0200

    Implemented generic repository pattern for data access

---
+++ src/CourseEnrollment.Data/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using CourseEnrollment.Data.Entities;
+using CourseEnrollment.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseEnrollment.Data.Repositories
+{
+    public class Repository<T>(ApplicationDbContext context) : IRepository<T> where T : BaseEntity
+    {
+        public virtual async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+            var entry = context.Set<T>().Entry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                context.Set<T>().Update(entity);
+            }
+
+            await context.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
