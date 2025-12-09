1d1f4454db3233479394e3b6efaa58fd9160590d
FlapJac-k <eslamce122@gmail.com>
2025-12-09 22:55:36 +0200

    Added mapping for StudentCourseEnrollment to EnrollmentViewModel

--- src/CourseEnrollment/Mappings/MappingProfile.cs
+++ src/CourseEnrollment/Mappings/MappingProfile.cs
@@ -19,6 +19,11 @@ namespace CourseEnrollment.Mappings
 
             CreateMap<CourseViewModel, Course>()
                 .ForMember(dest => dest.Enrollments, opt => opt.Ignore());
+
+            CreateMap<StudentCourseEnrollment, EnrollmentViewModel>()
+                .ForMember(dest => dest.StudentName, opt => opt.MapFrom(src => src.Student.FullName))
+                .ForMember(dest => dest.CourseTitle, opt => opt.MapFrom(src => src.Course.Title))
+                .ForMember(dest => dest.AvailableSlots, opt => opt.Ignore());
         }
     }
 }
