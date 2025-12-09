0291901a194271deace20574fed4567db0854bf5
FlapJac-k <eslamce122@gmail.com>
2025-12-09 20:58:30 +0200

    Added StudentViewModel

---
+++ src/CourseEnrollment/Models/StudentViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseEnrollment.Models
+{
+    public class StudentViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Full name is required")]
+        [Display(Name = "Full Name")]
+        public required string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Birthdate is required")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Birthdate")]
+        public DateTime Birthdate { get; set; }
+
+        [Required(ErrorMessage = "National ID is required")]
+        [StringLength(14, ErrorMessage = "National ID must be maximum 14 characters")]
+        [Display(Name = "National ID")]
+        public required string NationalId { get; set; }
+
+        [StringLength(11, ErrorMessage = "Phone number must be maximum 11 characters")]
+        [Display(Name = "Phone Number")]
+        public string? PhoneNumber { get; set; }
+    }
+}
